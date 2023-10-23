9251dc3e3c2d29d0903c4c97aa453a577ffc4f9e
mamantube <firmanhiday.id007@gmail.com>
2023-10-23 13:27:22 +0700

    try

--- tugas_javascript/callback/script.js
+++ tugas_javascript/callback/script.js
@@ -4,30 +4,22 @@ function renderTable(data) {
     data.array.forEach(element => {
         const row = tableBody.insertRow();
         row.insertCell().textContent = element.id;
+        row.insertCell().textContent = element.name;
+        row.insertCell().textContent = element.username;
+        row.insertCell().textContent = element.email;
+        row.insertCell().textContent = element.address;
+        row.insertCell().textContent = element.phone;
+        row.insertCell().textContent = element.website;
+        row.insertCell().textContent = element.company;
     });
 }
-
-// function getData(url, cb) {
-//     let udata = new XMLHttpRequest();
-//     udata.onload = function() {
-//         if (udata.status === 200) {
-//             // console.log(JSON.parse(udata.responseText));
-//             return cb(JSON.parse(udata.responseText));
-//         } else {
-//             console.log("data tidak ditemukan")
-//         }
-//     };
-//     udata.open("GET", url);
-//     udata.send()
-// }
 
 function getData(url, cb) {
     let udata = new XMLHttpRequest();
     udata.onload = function() {
         if (udata.status === 200) {
             // console.log(JSON.parse(udata.responseText));
-            const dataJson = JSON.parse(udata.responseText);
-            cb(dataJson);
+            return cb(JSON.parse(udata.responseText));
         } else {
             console.log("data tidak ditemukan")
         }
@@ -36,12 +28,27 @@ function getData(url, cb) {
     udata.send()
 }
 
+// function getData(url, cb) {
+//     let udata = new XMLHttpRequest();
+//     udata.onload = function() {
+//         if (udata.status === 200) {
+//             // console.log(JSON.parse(udata.responseText));
+//             const dataJson = JSON.parse(udata.responseText);
+//             cb(dataJson);
+//         } else {
+//             console.log("data tidak ditemukan")
+//         }
+//     };
+//     udata.open("GET", url);
+//     udata.send()
+// }
+
 getData("https://jsonplaceholder.typicode.com/users", renderTable);
 
 
-// const tData = getData("https://jsonplaceholder.typicode.com/users", function(tData) {
-//     console.log(tData[0])
-// });
+const tData = getData("https://jsonplaceholder.typicode.com/users", function(tData) {
+    console.log(tData[0])
+});
 
 // function populateTable(tData) {
 //     const tableBody = document.querySelector('#data-table tbody');
