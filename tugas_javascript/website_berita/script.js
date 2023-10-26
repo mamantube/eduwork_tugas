58bcd0be1c2295ec4bdc34ab888c1264e782ffb1
mamantube <firmanhiday.id007@gmail.com>
2023-10-26 17:08:11 +0700

    add search function

--- tugas_javascript/website_berita/script.js
+++ tugas_javascript/website_berita/script.js
@@ -1,25 +1,86 @@
-const apiKey = "7b328877d534470483ca958f359d1037";
-const apiUrl = "https://newsapi.org/v2/top-headlines";
-const country = "id"
+const newsContainer = document.getElementById("news-container")
+const searchInput = document.getElementById('search-input');
+const searchButton = document.getElementById('search-button');
 
-fetch(`${apiUrl}?country=${country}&apiKey=${apiKey}`)
-    .then(Response => Response.json())
+fetch("https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7b328877d534470483ca958f359d1037")
+    .then(response => response.json())
     .then(data => {
-        const newsBox = document.getElementById("news-container");
+        console.log(data.articles);
+        const newsContainer = document.getElementById('news-container');
         const articles = data.articles;
 
         articles.forEach(article => {
-            const newsItem = document.createElement("div");
-            newsItem.className = "news-item";
+            const newsItem = document.createElement('div');
+            newsItem.className = 'news-item m-4 d-flex';
             newsItem.innerHTML = `
-            <h2>${article.title}</h2>
-            <p>${article.description}</p>
-            <a href="${article.url}" target="_blank">Baca selengkapnya</a>
+                <div class="row row-cols-1 row-cols-md-3 g-4">
+                    <div class="col">
+                        <div class="card">
+                            <img class="card-img-top" src="${article.urlToImage}" alt="">
+                            <div class="card-body">
+                                <h2 class="card-title">${article.title}</h2>
+                                <p class="card-text">${article.description}</p>
+                                <p class="text-body-tertiary"><span class="fst-italic">${article.author}</span> </br> ${article.publishedAt}</p>
+                                <div></div>
+                                <a href="${article.url}" class="btn btn-primary" target="_blank">Baca selengkapnya</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             `;
-            newsBox.appendChild(newsItem);
-        })
+            newsContainer.appendChild(newsItem);
+        });
+        
     })
     .catch(error => {
         console.error("Error:", error);
     });
+
+function newsSearch () {
+    const searchTerm = searchInput.value;
+    fetch(`https://newsapi.org/v2/everything?domains=wsj.com&apiKey=7b328877d534470483ca958f359d1037`)
+        .then(Response => Response.json())
+        .then(data => {
+            console.log(data.articles)
+            newsContainer.innerHTML = "";
+
+            const articles = data.articles;
+            articles.forEach(article => {
+                const newsItem = document.createElement('div');
+                newsItem.className = 'news-item m-4 d-flex';
+                newsItem.innerHTML = `
+                    <div class="row row-cols-1 row-cols-md-3 g-4">
+                        <div class="col">
+                            <div class="card">
+                                <img class="card-img-top" src="${article.urlToImage}" alt="">
+                                <div class="card-body">
+                                    <h2 class="card-title">${article.title}</h2>
+                                    <p class="card-text">${article.description}</p>
+                                    <p class="text-body-tertiary"><span class="fst-italic">${article.author}</span> </br> ${article.publishedAt}</p>
+                                    <div></div>
+                                    <a href="${article.url}" class="btn btn-primary" target="_blank">Baca selengkapnya</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                `;
+                newsContainer.appendChild(newsItem)
+            });
+        })
+        .catch(error => {
+            console.error("Error", error);
+        });
+}
+
+searchButton.addEventListener("click", newsSearch);
+
+searchInput.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        newsSearch();
+    }
+});
+
     
+
+
+// my news api KEY = 7b328877d534470483ca958f359d1037
